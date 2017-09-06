55f0623b07f9dfc2b913d23cb7b62f2a1d96f321
piseth <sokpiseth777@gmail.com>
2017-09-06 02:25:09 -0400

    add typo3_src-6.2.6

---
+++ typo3_src-6.2.6/typo3/sysext/belog/Classes/ViewHelpers/WorkspaceTitleViewHelper.php
@@ -0,0 +1,53 @@
+<?php
+namespace TYPO3\CMS\Belog\ViewHelpers;
+
+/**
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+/**
+ * Get workspace title from workspace id
+ *
+ * @author Christian Kuhn <lolli@schwarzbu.ch>
+ */
+class WorkspaceTitleViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper {
+
+	/**
+	 * @var \TYPO3\CMS\Belog\Domain\Repository\WorkspaceRepository
+	 * @inject
+	 */
+	protected $workspaceRepository = NULL;
+
+	/**
+	 * Resolve workspace title from UID.
+	 *
+	 * @param integer $uid UID of the workspace
+	 * @return string username or UID
+	 */
+	public function render($uid) {
+		if ($uid === 0) {
+			return \TYPO3\CMS\Extbase\Utility\LocalizationUtility::translate('live', $this->controllerContext->getRequest()->getControllerExtensionName());
+		}
+		if (!\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('workspaces')) {
+			return '';
+		}
+		/** @var $workspace \TYPO3\CMS\Belog\Domain\Model\Workspace */
+		$workspace = $this->workspaceRepository->findByUid($uid);
+		if ($workspace !== NULL) {
+			$title = $workspace->getTitle();
+		} else {
+			$title = '';
+		}
+		return $title;
+	}
+
+}
---
+++ typo3_src-6.2.6/typo3/sysext/core/Classes/Resource/Processing/LocalPreviewHelper.php
@@ -0,0 +1,84 @@
+<?php
+namespace TYPO3\CMS\Core\Resource\Processing;
+
+/**
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+use \TYPO3\CMS\Core\Resource, \TYPO3\CMS\Core\Utility;
+
+/**
+ * Helper for creating local image previews using TYPO3s image processing classes.
+ */
+class LocalPreviewHelper {
+	/**
+	 * @var LocalImageProcessor
+	 */
+	protected $processor;
+
+	/**
+	 * @param LocalImageProcessor $processor
+	 */
+	public function __construct(LocalImageProcessor $processor) {
+		$this->processor = $processor;
+	}
+
+	/**
+	 * This method actually does the processing of files locally
+	 *
+	 * takes the original file (on remote storages this will be fetched from the remote server)
+	 * does the IM magic on the local server by creating a temporary typo3temp/ file
+	 * copies the typo3temp/ file to the processing folder of the target storage
+	 * removes the typo3temp/ file
+	 *
+	 * @param TaskInterface $task
+	 * @return array
+	 */
+	public function process(TaskInterface $task) {
+		$targetFile = $task->getTargetFile();
+		$sourceFile = $task->getSourceFile();
+
+			// Merge custom configuration with default configuration
+		$configuration = array_merge(array('width' => 64, 'height' => 64), $task->getConfiguration());
+		$configuration['width'] = Utility\MathUtility::forceIntegerInRange($configuration['width'], 1);
+		$configuration['height'] = Utility\MathUtility::forceIntegerInRange($configuration['height'], 1);
+
+		$originalFileName = $sourceFile->getForLocalProcessing(FALSE);
+
+			// Create a temporaryFile
+		$temporaryFileName = Utility\GeneralUtility::tempnam('preview_', '.' . $task->getTargetFileExtension());
+			// Check file extension
+		if ($sourceFile->getType() != Resource\File::FILETYPE_IMAGE &&
+			!Utility\GeneralUtility::inList($GLOBALS['TYPO3_CONF_VARS']['GFX']['imagefile_ext'], $sourceFile->getExtension())) {
+				// Create a default image
+			$this->processor->getTemporaryImageWithText($temporaryFileName, 'Not imagefile!', 'No ext!', $sourceFile->getName());
+		} else {
+				// Create the temporary file
+			if ($GLOBALS['TYPO3_CONF_VARS']['GFX']['im']) {
+				$parameters = '-sample ' . $configuration['width'] . 'x' . $configuration['height'] . ' '
+					. $this->processor->wrapFileName($originalFileName) . '[0] ' . $this->processor->wrapFileName($temporaryFileName);
+
+				$cmd = Utility\GeneralUtility::imageMagickCommand('convert', $parameters) . ' 2>&1';
+				Utility\CommandUtility::exec($cmd);
+
+				if (!file_exists($temporaryFileName)) {
+						// Create a error gif
+					$this->processor->getTemporaryImageWithText($temporaryFileName, 'No thumb', 'generated!', $sourceFile->getName());
+				}
+			}
+		}
+
+		return array(
+			'filePath' => $temporaryFileName,
+		);
+	}
+}
